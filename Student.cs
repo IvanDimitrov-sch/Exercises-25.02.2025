7cdcdd01706f4003d1fb3c12ed12026c236dbc52
MEGA <MEGA@gmail.com>
2025-02-25 17:56:16 +0200

    Classes done

---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_25._02._2025
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public Student(string name, int score)
+        {
+            this.Name = name;
+            this.Score = score;
+        }
+
+
+    }
+}
---
+++ University.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises_25._02._2025
+{
+    internal class University
+    {
+        List<Student> students;//sorted
+        public string FindStudentByScore(int score)
+        {
+            int left = 9;
+            int right = students.Count - 1;
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                if (students[mid].Score == score)
+                {
+                    return students[mid].Name;
+                }
+                else if (students[mid].Score < score)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return "Not found";
+        }
+    }
+}
