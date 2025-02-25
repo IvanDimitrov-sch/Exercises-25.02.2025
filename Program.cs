1f439c910175743f2e227edba14d62d9db2917b3
MEGA <MEGA@gmail.com>
2025-02-25 17:26:02 +0200

    Transposing

--- Program.cs
+++ Program.cs
@@ -6,5 +6,31 @@
         {
             
         }
+        public static int[] CreateArray(int size, bool sorted)
+        {
+            Random random = new Random(Environment.TickCount);
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next(0, size);
+            }
+            if (sorted)
+            {
+                Array.Sort(array);
+            }
+            return array;
+        }
+        public static Array Transposed(int[] array, int key)
+        {
+            for(int i =0; i < array.Length;i++)
+            {
+                if(array[i] == key)
+                {
+                    (array[i], array[i-1]) = (array[i-1], array[i]);
+                }
+            }
+            return array;
+        }
+        
     }
 }
