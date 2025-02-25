a02be6c2d1cd1d924c282eafa659fd20adb3c828
MEGA <MEGA@gmail.com>
2025-02-25 17:37:18 +0200

    DONE

--- Program.cs
+++ Program.cs
@@ -4,10 +4,18 @@
     {
         static void Main(string[] args)
         {
-            
+            int[] array = CreateArray(10, false);
+            Console.WriteLine("Enter key: \n");
+            int k = int.Parse(Console.ReadLine());
+            Console.WriteLine("-----ARRAY IS-----\n" + String.Join(",", array));
+            Console.WriteLine("---------------------\n");
+            Console.WriteLine(String.Join(",", Transposed(array, k)));
+            Console.WriteLine("---------------------\n");
+            Console.WriteLine(String.Join(",", Moving(array, k)));
         }
         public static int[] CreateArray(int size, bool sorted)
         {
+            
             Random random = new Random(Environment.TickCount);
             int[] array = new int[size];
             for (int i = 0; i < size; i++)
@@ -18,26 +26,31 @@
             {
                 Array.Sort(array);
             }
+            
             return array;
         }
-        public static Array Transposed(int[] array, int key)
+        public static int[] Transposed(int[] array, int key)
         {
-            for(int i =0; i < array.Length;i++)
+            Console.WriteLine("-----TRANSPOSING-----");
+            for (int i =0; i < array.Length;i++)
             {
                 if(array[i] == key)
                 {
                     (array[i], array[i-1]) = (array[i-1], array[i]);
+                    return array;
                 }
             }
             return array;
         }
-        public static Array Moving(int[] array, int key)
+        public static int[] Moving(int[] array, int key)
         {
+            Console.WriteLine("-----MOVING-----");
             for (int i = 0; i < array.Length; i++)
             {
                 if (array[i] == key)
                 {
                     (array[0], array[i]) = (array[i], array[0]);
+                    return array;
                 }
             }
             return array;
